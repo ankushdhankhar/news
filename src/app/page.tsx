44780407f23183a7e0fb169d7ee7ddb7297b49b3
Anit <anitjha31@gmail.com>
2025-01-04 12:12:43 +0530

    fix: add new section on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import FlipFlopSection from "@/components/flip-flop-section";
+import NewsPage from "@/components/news-grid";
 import NewsCarousel from "@/components/newscarousel";
 import {
   getAllFlipNews,
@@ -10,13 +11,18 @@ import {
 export default async function Home() {
   const { data: news } = await getPaginatedNews(1, 6);
   const { data: flipnews } = await getPaginatedFlipNews(1, 3);
+  const { data: politicsNews } = await getPaginatedNews(1, 3, "politics");
   // console.log(news);
   news.sort((a, b) => (new Date(a.time) > new Date(b.time) ? -1 : 1));
   flipnews.sort((a, b) => (new Date(a.time) > new Date(b.time) ? -1 : 1));
   return (
-    <main className="min-h-screen bg-gray-50">
+    <main className="min-h-screen bg-gray-50 max-w-7xl m-auto">
       <NewsCarousel news={news} />
       <FlipFlopSection news={flipnews} />
+      <div className="px-8">
+        <h2 className="text-center text-2xl">Politics</h2>
+        <NewsPage news={politicsNews} />
+      </div>
     </main>
   );
 }
--- src/components/news-grid.tsx
+++ src/components/news-grid.tsx
@@ -38,7 +38,7 @@ export default function NewsPage({ news }: { news: INews[] }) {
         categories={categories}
         onFilterChange={handleFilterChange}
       /> */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 place-items-center">
         {filteredNews.map((item) => (
           <NewsCard key={item.id} news={item} />
         ))}
